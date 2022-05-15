e3e025d3d872e3f207cd5d199bb79dea03f7b1e2
Juan Betancourt <juancbetancourt11@gmail.com>
2022-05-15 20:12:13 +0000

    Learning Dictionary key and value

--- main.py
+++ main.py
@@ -5,7 +5,26 @@ programming_dictionary = {
 }
 
 #Retrieving items from dictionary.
-print(programming_dictionary["Bug"])
+# print(programming_dictionary["Function"])
 
 #Adding new items to dictionary.
-progromming_dictionary
+# programming_dictionary["Loop"] = "The action of doing something over and over again."
+# print(programming_dictionary)
+
+#Create an empty dictionary
+# empty_dictionary = {}
+# empty_dictionary["Loop"] = "The action of doing something over and over again."
+# print(empty_dictionary)
+
+#wipe an existing dictionary
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+#edit an item in a dictionary
+# programming_dictionary["Bug"] = "A moth in the computer" 
+# print(programming_dictionary)
+
+#Loop through a dictionary
+for key in programming_dictionary:
+  print(key)
+  print(programming_dictionary[key])
